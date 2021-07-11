a3370fb084567f17d0ef129f65685eab906258b3
jseok.heo <jseok.heo@gmail.com>
2021-07-11 15:45:50 +0900

    Updated SCCB Master and its testbench.

--- RTL/OV7670_CTRL/SCCB_MST.v
+++ RTL/OV7670_CTRL/SCCB_MST.v
@@ -1,139 +1,157 @@
-
-
-// 1. the master device must be able to support and maintain the data line of the bus in a tri-state mode
-// 2. the alternative method if the master cannot maintain a tri-state condition of the data line is to drive
-//    the data line either high or low and to note the transition there to assert communications with the slave
-//    CAMERA CHIP
-
-// O_SCCB_E_N : Serial Chip Select Output (Where SCCB_E is not present on the CAMERA CHIP, this signal is by default enabled and held high)
-//              Indicates the start or stop of the data transmission. (Active-low, Driven by Master only)
-//              H -> L : a start of transmission.
-//              L -> H : a stop  of transmission.
-//              Drives at logical 1       : the bus is idle.
-//              Drives at logical 0       : the master asserts transmissions or the system is in Suspend mode.
-//              => SCCB_E must remain at logical 0 during a data transmission
-
-// O_SIO_C    : Serial I/O Signal 1 Output
-//              Indicates each transmitted bit. (Active-high, Driven by Master only)
-//              Data transmission starts when SIO_C is driven at logical 0 after the start of transmission.
-//              logical 1 of SIO_C during a data transmission indicates a single transmitted bit.
-//              Thus, SIO_D can occur only when SIO_C is driven at 0.
-//              The period of a signle transmitted bit is defined as tCYC as shown in Figure 3-8.
-//              The minimum of tCYC is 10us.
-//              Drives at logical 1       : the bus is idle.
-//              Drives at logical 0 and 1 : O_SCCB_E is driven at 0.
-
-// IO_SIO_D   : Serial I/O Signal 0 input and Output
-//              Driven by either Master or Slave)
-//              The master must avoid propagating an unknown bus state condition when the bus is floating or conflicting
-//              A conflict-protection resistor is required to redute static current when the bus conflicts (Figure 4-2)
-//              A single-bit transmission is indicated by a logical 1 of O_SIO_C
-//              SIO_D can occur only when SIO_C is driven at logical 0
-//              An exception is allowed at the beginning and the end of a transmission.
-//              During the period that SCCB_E is asserted and before SIO_C goes to 0, SIO_D can be driven at 0
-//              During the period that SIO_C goes to 1 and before SCCB_E is de-asserted, SIO_D can also be driven at 0
-//              Bus float and contention are allowed during transmission of Dont-Care or NA bits.
-//              Remains floating          : the bus is idle.
-//              Drives at logical 0       : the system is Suspend mode.
-// O_PWDN     : Power down output
+/* 
+ * SCCB Master
+ * Copyright (C) 2021, Jaeseok Heo (jseok.heo@gmail.com)
+ *                                 (jseok_heo@korea.ac.kr)
+ * 
+ * Description:     SCCB Master Interface for controlling OV7670 Camera module 
+ *                  Supports Write Transmission only
+ *                  Supports Single slave device only
+ * History:              
+ *          2021.07.11 - Initial release
+ */
 
 module SCCB_MST
 (
      input          I_CLK
     ,input          I_RST_N
-    ,input [23:0]   I_DATA
-    // I2C Interface
-    ,output         O_SCCB_E_N       // Not available with only single slave device
-    ,output         O_SIO_C
-    ,inout          IO_SIO_D
+    ,input [31:0]   I_DATA              // Sync to I_CLK
+    ,input          I_START             // Sync to I_CLK
+    ,output         O_BUSY              // Sync to I_CLK
+    ,output         O_WAIT_INTR_CLR     // Sync to I_CLK
+    ,input          I_INTR_CLR          // Sync to I_CLK, Interrupt clear
+    // SCCB Interface
+    ,output         O_SCCB_E_N          // Not available with only single slave device
+    ,output         O_SIO_C             // SCL
+    ,inout          IO_SIO_D            // SDA
 );
     
     localparam H = 1'b1;
     localparam L = 1'b0;
+    localparam STATE_IDLE = 2'd0;
+    localparam STATE_RUN  = 2'd1;
+    localparam STATE_INTR = 2'd2;
 
-    //10101011_11001101_11101111
-    wire        w_trans_type = I_DATA[16];       // 0 for write, 1 for read
-    reg  [9:0]  r_state_cnt;
+    reg  [ 5:0] r_phase_bit_cnt;
+    reg  [31:0] r_data;
+    reg         r_busy;
     reg         r_scl;
     reg         r_sda;
     reg         r_sio_d_oe_m_n;
-//    wire        w_is_wr_done;
-//    wire        w_is_rd_done;
+    wire        w_is_wr_done;
     wire        w_is_data_valid;
+    wire        w_exit_STATE_IDLE;
+    wire        w_exit_STATE_RUN;
+    wire        w_exit_STATE_INTR;
 
-//    wire        w_is_wr_started = ~w_trans_type;
-//    wire        w_is_rd_started = w_trans_type;
+    reg  [ 1:0] r_state;
+    reg  [ 1:0] r_next_state;
+    reg         r_start;
+    reg         r_intr_clr;
 
-    assign w_is_data_valid = r_state_cnt >= 10'd3 && r_state_cnt <= 10'd29;
+    always @(posedge I_CLK, negedge I_RST_N)
+        if(!I_RST_N)    r_state <= STATE_IDLE;
+        else            r_state <= r_next_state;
+
+    always @(*) begin
+             if(!I_RST_N)           r_next_state = STATE_IDLE;
+        else if(w_exit_STATE_IDLE)  r_next_state = STATE_RUN;
+        else if(w_exit_STATE_RUN)   r_next_state = STATE_INTR;
+        else if(w_exit_STATE_INTR)  r_next_state = STATE_IDLE;
+    end
+
+    always @(posedge I_CLK, negedge I_RST_N)
+             if(!I_RST_N)               r_data <= 32'h0;
+        else if(r_state == STATE_IDLE)  r_data <= I_DATA;
+
+    assign O_BUSY = r_state == STATE_RUN;
+
+    assign O_WAIT_INTR_CLR = r_state == STATE_INTR;
+
+    always @(posedge I_CLK, negedge I_RST_N)
+             if(!I_RST_N)               r_start <= L;
+        else if(w_exit_STATE_IDLE)      r_start <= L;
+        else if(r_state == STATE_IDLE)  r_start <= I_START;
+
+    always @(posedge I_CLK, negedge I_RST_N)
+             if(!I_RST_N)               r_intr_clr <= L;
+        else if(w_exit_STATE_INTR)      r_intr_clr <= L;
+        else if(r_state == STATE_INTR)  r_intr_clr <= I_INTR_CLR;
+        else                            r_intr_clr <= L;
+
+    assign w_exit_STATE_IDLE = r_start      & (r_state == STATE_IDLE);
+    assign w_exit_STATE_RUN  = w_is_wr_done & (r_state == STATE_RUN );
+    assign w_exit_STATE_INTR = r_intr_clr   & (r_state == STATE_INTR);
+
+    assign w_is_wr_done = r_phase_bit_cnt == 6'd33;
+
+    assign w_is_data_valid   = (r_phase_bit_cnt >= 6'd3) && (r_phase_bit_cnt <= 6'd29);
 
 
     always @(posedge I_CLK, negedge I_RST_N)
-             if(!I_RST_N)           r_state_cnt <= 10'h0;
-//        else if(w_is_wr_done)       r_state_cnt <= 10'h0;
-//        else if(w_is_rd_done)       r_state_cnt <= 10'h0;
-//        else if(w_is_wr_started)    r_state_cnt <= 10'h0;
-//        else if(w_is_rd_started)    r_state_cnt <= 10'h0;
-        else                        r_state_cnt <= r_state_cnt + 10'd1;
+        if(!I_RST_N)                    r_phase_bit_cnt <= 10'h0;
+        else if(w_exit_STATE_RUN)       r_phase_bit_cnt <= 10'h0;
+        else if(r_state == STATE_RUN)   r_phase_bit_cnt <= r_phase_bit_cnt + 6'd1;
 
     always @(*) begin
-             if(!I_RST_N)               r_scl = H;
-        else if(r_state_cnt == 10'd0)   r_scl = H;
-        else if(r_state_cnt == 10'd1)   r_scl = H;
-        else if(r_state_cnt == 10'd2)   r_scl = L;
-        else if(r_state_cnt == 10'd30)  r_scl = H;
-        else if(r_state_cnt == 10'd31)  r_scl = H;
-        else                            r_scl = L;
+             if(!I_RST_N)                   r_scl = H;
+        else if(r_phase_bit_cnt == 6'd0 )   r_scl = H;
+        else if(r_phase_bit_cnt == 6'd1 )   r_scl = H;
+        else if(r_phase_bit_cnt == 6'd2 )   r_scl = L;
+        else if(r_phase_bit_cnt == 6'd30)   r_scl = H;
+        else if(r_phase_bit_cnt == 6'd31)   r_scl = H;
+        else                                r_scl = L;
     end
 
     always @(*) begin
-             if(!I_RST_N)               r_sda = H;
-        else if(r_state_cnt == 10'd0)   r_sda = H;
-        else if(r_state_cnt == 10'd1)   r_sda = L;
+             if(!I_RST_N)                   r_sda = H;
+        else if(r_phase_bit_cnt == 6'd0 )   r_sda = H;
+        else if(r_phase_bit_cnt == 6'd1 )   r_sda = L;
         // ID ADDR
-        else if(r_state_cnt == 10'd3)   r_sda = I_DATA[23];
-        else if(r_state_cnt == 10'd4)   r_sda = I_DATA[22];
-        else if(r_state_cnt == 10'd5)   r_sda = I_DATA[21];
-        else if(r_state_cnt == 10'd6)   r_sda = I_DATA[20];
-        else if(r_state_cnt == 10'd7)   r_sda = I_DATA[19];
-        else if(r_state_cnt == 10'd8)   r_sda = I_DATA[18];
-        else if(r_state_cnt == 10'd9)   r_sda = I_DATA[17];
-        else if(r_state_cnt == 10'd10)  r_sda = I_DATA[16];
-        else if(r_state_cnt == 10'd11)  r_sda = H; // ACK
+        else if(r_phase_bit_cnt == 6'd3 )   r_sda = I_DATA[23];
+        else if(r_phase_bit_cnt == 6'd4 )   r_sda = I_DATA[22];
+        else if(r_phase_bit_cnt == 6'd5 )   r_sda = I_DATA[21];
+        else if(r_phase_bit_cnt == 6'd6 )   r_sda = I_DATA[20];
+        else if(r_phase_bit_cnt == 6'd7 )   r_sda = I_DATA[19];
+        else if(r_phase_bit_cnt == 6'd8 )   r_sda = I_DATA[18];
+        else if(r_phase_bit_cnt == 6'd9 )   r_sda = I_DATA[17];
+        else if(r_phase_bit_cnt == 6'd10)   r_sda = I_DATA[16];
+        else if(r_phase_bit_cnt == 6'd11)   r_sda = H; // Phase 0 ACK
         // SUB ADDR
-        else if(r_state_cnt == 10'd12)  r_sda = I_DATA[15];
-        else if(r_state_cnt == 10'd13)  r_sda = I_DATA[14];
-        else if(r_state_cnt == 10'd14)  r_sda = I_DATA[13];
-        else if(r_state_cnt == 10'd15)  r_sda = I_DATA[12];
-        else if(r_state_cnt == 10'd16)  r_sda = I_DATA[11];
-        else if(r_state_cnt == 10'd17)  r_sda = I_DATA[10];
-        else if(r_state_cnt == 10'd18)  r_sda = I_DATA[ 9];
-        else if(r_state_cnt == 10'd19)  r_sda = I_DATA[ 8];
-        else if(r_state_cnt == 10'd20)  r_sda = H; // ACK
+        else if(r_phase_bit_cnt == 6'd12)   r_sda = I_DATA[15];
+        else if(r_phase_bit_cnt == 6'd13)   r_sda = I_DATA[14];
+        else if(r_phase_bit_cnt == 6'd14)   r_sda = I_DATA[13];
+        else if(r_phase_bit_cnt == 6'd15)   r_sda = I_DATA[12];
+        else if(r_phase_bit_cnt == 6'd16)   r_sda = I_DATA[11];
+        else if(r_phase_bit_cnt == 6'd17)   r_sda = I_DATA[10];
+        else if(r_phase_bit_cnt == 6'd18)   r_sda = I_DATA[ 9];
+        else if(r_phase_bit_cnt == 6'd19)   r_sda = I_DATA[ 8];
+        else if(r_phase_bit_cnt == 6'd20)   r_sda = H; // Phase 1 ACK
         // WDATA
-        else if(r_state_cnt == 10'd21)  r_sda = I_DATA[ 7];
-        else if(r_state_cnt == 10'd22)  r_sda = I_DATA[ 6];
-        else if(r_state_cnt == 10'd23)  r_sda = I_DATA[ 5];
-        else if(r_state_cnt == 10'd24)  r_sda = I_DATA[ 4];
-        else if(r_state_cnt == 10'd25)  r_sda = I_DATA[ 3];
-        else if(r_state_cnt == 10'd26)  r_sda = I_DATA[ 2];
-        else if(r_state_cnt == 10'd27)  r_sda = I_DATA[ 1];
-        else if(r_state_cnt == 10'd28)  r_sda = I_DATA[ 0];
-        else if(r_state_cnt == 10'd29)  r_sda = H; // ACK
-        else if(r_state_cnt == 10'd30)  r_sda = L;
-        else if(r_state_cnt == 10'd32)  r_sda = H;
-        else                            r_sda = L;
+        else if(r_phase_bit_cnt == 6'd21)   r_sda = I_DATA[ 7];
+        else if(r_phase_bit_cnt == 6'd22)   r_sda = I_DATA[ 6];
+        else if(r_phase_bit_cnt == 6'd23)   r_sda = I_DATA[ 5];
+        else if(r_phase_bit_cnt == 6'd24)   r_sda = I_DATA[ 4];
+        else if(r_phase_bit_cnt == 6'd25)   r_sda = I_DATA[ 3];
+        else if(r_phase_bit_cnt == 6'd26)   r_sda = I_DATA[ 2];
+        else if(r_phase_bit_cnt == 6'd27)   r_sda = I_DATA[ 1];
+        else if(r_phase_bit_cnt == 6'd28)   r_sda = I_DATA[ 0];
+        else if(r_phase_bit_cnt == 6'd29)   r_sda = H; // Phase 2 ACK
+        else if(r_phase_bit_cnt == 6'd30)   r_sda = L;
+        else if(r_phase_bit_cnt == 6'd32)   r_sda = H;
+        else                                r_sda = L;
     end
 
     always @(*) begin
-             if(r_state_cnt == 10'd11)  r_sio_d_oe_m_n = H;   // ACK
-        else if(r_state_cnt == 10'd20)  r_sio_d_oe_m_n = H;   // ACK
-        else if(r_state_cnt == 10'd29)  r_sio_d_oe_m_n = H;   // ACK
-        else                            r_sio_d_oe_m_n = L;
+             if(r_phase_bit_cnt == 6'd11)   r_sio_d_oe_m_n = H;   // Phase 0 ACK => SIO D is used as input
+        else if(r_phase_bit_cnt == 6'd20)   r_sio_d_oe_m_n = H;   // Phase 1 ACK => SIO D is used as input
+        else if(r_phase_bit_cnt == 6'd29)   r_sio_d_oe_m_n = H;   // Phase 2 ACK => SIO D is used as input
+        else                                r_sio_d_oe_m_n = L;
     end
 
     assign O_SIO_C  = r_scl | w_is_data_valid ? ~I_CLK : L;
     assign IO_SIO_D = r_sio_d_oe_m_n ? 1'hz : r_sda;
 //    assign O_SCCB_E_N = r_state == IDLE ? H : L;
+    assign O_SCCB_E_N = H;
 
 endmodule
 
--- SIM/test_sccb_wr/testbench.sv
+++ SIM/test_sccb_wr/testbench.sv
@@ -1,35 +1,81 @@
+class packet;
+    rand bit [7:0] r_id_addr    ;
+    rand bit [7:0] r_sub_addr   ;
+    rand bit [7:0] r_byte_data  ;
+
+    constraint trans_type { r_id_addr[0] == 0; }
+
+    function bit [31:0] get_packet();
+        get_packet = {r_id_addr, r_sub_addr, r_byte_data, 8'h00};
+    endfunction : get_packet
+
+    function void display();
+        $display("ID_ADDR : %x, SUB_ADDR : %x, DATA : %x", r_id_addr, r_sub_addr, r_byte_data);
+    endfunction : display
+
+endclass: packet
+
 module testbench;
 
-    reg         r_clk       ;
-    reg         r_rst_n     ;
-    reg [23:0]  r_data      ;
-    wire        w_sccb_e_n  ;
-    wire        w_sio_c     ;
-    wire        w_sio_d     ;
+    reg         r_clk           ;
+    reg         r_rst_n         ;
+    reg  [31:0] r_data          ;
+    reg         r_start         ;
+    wire        w_busy          ;
+    wire        w_wait_intr_clr ;
+    reg         r_intr_clr      ;
+    wire        w_sccb_e_n      ;
+    wire        w_sio_c         ;
+    wire        w_sio_d         ;
 
     localparam CLK_PERIOD = 40;
 
     SCCB_MST Inst_SCCB_MST (
-         .I_CLK         (   r_clk                  )   // input          I_CLK
-        ,.I_RST_N       (   r_rst_n                )   //,input          I_RST_N
-        ,.I_DATA        (   r_data                 )   //,input [23:0]   I_DATA
-        ,.O_SCCB_E_N    (   w_sccb_e_n             )   //,output         O_SCCB_E_N       // Not available with only single slave device
-        ,.O_SIO_C       (   w_sio_c                )   //,output         O_SIO_C
-        ,.IO_SIO_D      (   w_sio_d                )   //,inout          IO_SIO_D
+         .I_CLK             (   r_clk           )   // input          I_CLK
+        ,.I_RST_N           (   r_rst_n         )   //,input          I_RST_N
+        ,.I_DATA            (   r_data          )   //,input [23:0]   I_DATA
+        ,.I_START           (   r_start         )   //.input          I_START
+        ,.O_BUSY            (   w_busy          )   //.output         O_BUSY
+        ,.O_WAIT_INTR_CLR   (   w_wait_intr_clr )   //.output         O_WAIT_INTR_CLR
+        ,.I_INTR_CLR        (   r_intr_clr      )   //.input          I_INTR_CLR
+        ,.O_SCCB_E_N        (   w_sccb_e_n      )   //,output         O_SCCB_E_N       // Not available with only single slave device
+        ,.O_SIO_C           (   w_sio_c         )   //,output         O_SIO_C
+        ,.IO_SIO_D          (   w_sio_d         )   //,inout          IO_SIO_D
     );
+
+    packet _pkt = new();
 
     initial begin
         forever #(CLK_PERIOD/2) r_clk = ~r_clk;
     end
 
     initial begin
-        r_clk   = 'h0;
-        r_rst_n = 'h0;
-        r_data  = 'hab_cd_ef;
+        r_clk       = 'h0;
+        r_rst_n     = 'h0;
+        r_data      = 'h0;
+        r_start     = 'h0;
+        r_intr_clr  = 'h0;
+
 
         #200; r_rst_n = 'h1;
+        repeat(4) begin
+            repeat(100) @(posedge r_clk); #1;
+            _pkt.randomize();
+            _pkt.display();
+            r_data = _pkt.get_packet();
+            @(posedge r_clk); #1;
+            r_start    = 1;
+            r_intr_clr = 0;
 
-        #1000;
+            repeat(100) @(posedge r_clk);
+            if(w_wait_intr_clr == 1);
+            repeat(3) @(posedge r_clk);
+            #1;
+            r_intr_clr = 1;
+            r_start    = 0;
+            
+        end
+
         $finish();
     end
     
